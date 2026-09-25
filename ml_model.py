b8e3789935205f91fc3ffeb100eed38796092c1f
MACsepehri <macsepehri@gmail.com>
2026-09-25 12:22:54 +0330

    Added description for people who are reading the source.

--- ml_model.py
+++ ml_model.py
@@ -18,14 +18,26 @@ class Reader:
     def content(self): return self.__content
 
 class ml_model:
+    '''
+        ml_model() class is the main class for generating a offer or checking does user need offer or not.
+    '''
     def __init__(self, product_data_filepath='all.csv', user_data_filepath='user.csv'):
         self.products = Reader(product_data_filepath)
         self.users = Reader(user_data_filepath)
 
     def need_offer(self, user_data):
-        if len(list(user_data[::-1][0]))+len(list(user_data[::-1][1])) < 2:
-            return False
+        '''
+            this function is for checking the user data that does him/her need offer or not.
+            it checks by the user in_cart / bought_it index in csv data.
+            it is optional to checking like this, you can change it to sth like check just by in_cart, it is optional. 
+        '''
+        if len(list(user_data[::-1][0])) < 1 or len(list(user_data[::-1][1])) < 1: #checks the in_cart and bought_it lengths
+            return False #user didn't bought sth or didn't have sth in his/her cart
         return True
 
     @property
-    def rand_user(self): return random.choice(self.users.content)
+    def rand_user(self):
+        '''
+            returns one of the users randomly from the csv data of users
+        '''
+        return random.choice(self.users.content)
